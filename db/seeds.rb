2c6f475f48a7c6eb3f5fb0673289fcacfd437563
Yuyi <yuyi365@gmail.com>
2021-12-11 13:15:05 -0500

    commit tree 11558a902075f7cc57af0a9d28ee5825392530a8

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: 'Yuyi', last_name: 'Li', grade: 20)
